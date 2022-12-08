0226e6f21fdbca358948d891676eacf83cfb5fc3
viktorasbuivydas <viktoras162@gmail.com>
2022-12-08 19:19:18 +0200

    role validation on mobile

--- middleware/notUser.js
+++ middleware/notUser.js
@@ -1,6 +1,6 @@
 export default function ({ $gates, $auth, redirect }) {
   // If the user is not an admin
-  if ($gates.hasRole("user")) {
+  if (!$gates.hasRole("super admin")) {
     return redirect("/home");
   }
 }
--- pages/home/chat/_room.vue
+++ pages/home/chat/_room.vue
@@ -238,14 +238,26 @@ const message = computed(() => {
 
 onMounted(async () => {
   getMessages(page.value);
-  $echo.private("chat." + chatId).listen("MessageSent", (event) => {
-    store.dispatch("chat/sendMessage", event.message);
-  });
+  // $echo.join("chat." + chatId).here((users) => {
+  //   console.log(users);
+  // })
+
+  $echo
+    .join("presence.chat." + chatId)
+    .joining((user) => {
+      console.log(user);
+    })
+    .leaving((user) => {
+      console.log(user);
+    })
+    .listen("MessageSent", (event) => {
+      store.dispatch("chat/sendMessage", event.message);
+    });
 });
 
 onBeforeUnmount(() => {
   store.dispatch("chat/setPage", 1);
-  $echo.leave("private-chat." + chatId);
+  // $echo.leave("private-chat." + chatId);
 });
 
 const emojiClick = (emoji) => {
